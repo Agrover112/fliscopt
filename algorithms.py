068c904929daeb6ba562963998ea2d5d6a3c9ff4
Ankit Grover <agrover112@gmail.com>
2021-06-22 16:26:52 +0530

    fix: Fix and sort imports

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,8 @@ import math
 import random
 import sys
 
+from ga_utils import crossover, mutation
+
 """Add option to select mutation type in GA method.
        Add bit flip mutation
        Wrapp all different mutation types in a class or file
@@ -188,48 +190,6 @@ def simulated_annealing(domain, fitness_function, seed=random.randint(10, 100), 
 
     print('Count: ', count)
     return solution, best, scores, nfe, seed
-
-
-def mutation(domain, step, solution):
-    gene = random.randint(0, len(domain)-1)
-    mutant = solution
-    if random.random() < 0.5:
-        if solution[gene] != domain[gene][0]:
-            mutant = solution[0:gene]+[solution[gene]-step]+solution[gene+1:]
-    else:
-        if solution[gene] != domain[gene][1]:
-            mutant = solution[0:gene]+[solution[gene]+step]+solution[gene+1:]
-    return mutant
-
-
-def multi_mutation(domain, step, solution):
-    li = [i for i in range(domain[0][0], domain[0][1]+1)]
-    gene = random.randint(0, len(domain)-1)
-    if gene in li:
-        li.remove(gene)
-    gene2 = random.choice(li)
-    mutant = solution
-    if random.random() < 0.5:
-        if solution[gene] != domain[gene][0]:
-            mutant = solution[0:gene]+[solution[gene]-step]+solution[gene+1:]
-    else:
-        if solution[gene] != domain[gene][1]:
-            mutant = solution[0:gene]+[solution[gene]+step]+solution[gene+1:]
-    if random.random() < 0.5:
-        if solution[gene2] != domain[gene2][0]:
-            mutant = solution[0:gene2] + \
-                [solution[gene2]-step]+solution[gene2+1:]
-    else:
-        if solution[gene2] != domain[gene2][1]:
-            mutant = solution[0:gene2] + \
-                [solution[gene2]+step]+solution[gene2+1:]
-
-    return mutant
-
-
-def crossover(domain, solution_1, solution_2):
-    gene = random.randint(1, len(domain) - 2)
-    return solution_1[0:gene] + solution_2[gene:]
 
 
 def genetic_algorithm(domain, fitness_function, seed=random.randint(10, 100), seed_init=True, init=[], population_size=100, step=1,
