c4eb965919703519a5e3f742d7ffdea25768ea38
Ankit Grover <agrover112@gmail.com>
2021-09-23 11:22:59 +0530

    fix: Added init version number

--- final/__init__.py
+++ final/__init__.py
@@ -1,10 +1,3 @@
 from . import ga,hc,rs,sa,multiproc,chaining,base_algorithm,fitness,utils
-"""from .ga import GA,GAReversals,ReverseGA
-from .hc import HillClimb
-from .sa import SimulatedAnnealing
-from .rs import RandomSearch
-from .multiproc import multiple_runs
-from .chaining import IteratedChaining
-from .utils import utils
-from .utils import ga_utils
-"""
+
+__version__ = "0.1.0"
