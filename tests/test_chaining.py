6d489937cc345810fc30c89a390b6be38fcefcec
Agrover112 <42321810+Agrover112@users.noreply.github.com>
2021-10-20 03:56:15 +0530

    fix: Updated chaining msg

--- tests/test_chaining.py
+++ tests/test_chaining.py
@@ -11,13 +11,13 @@ from fliscopt.chaining import IteratedChaining
 
 class TestChaining(unittest.TestCase):
     def test_iter_1(self): # An bug in eariler version caused this test to fail
-        self.assertIsNotNone(IteratedChaining(rounds=1, n_obs=2, tol=90).run('RandomSearch', 'HillClimb'))
+        self.assertIsNotNone(IteratedChaining(rounds=1, n_obs=2, tol=90).run('RandomSearch', 'HillClimb'), msg= "Iterated Chaining fails when no. of rounds =1 ")
     
     def test_iter_odd(self):
-        self.assertIsNotNone(IteratedChaining(rounds=3, n_obs=2, tol=90).run('RandomSearch', 'HillClimb'))
+        self.assertIsNotNone(IteratedChaining(rounds=3, n_obs=2, tol=90).run('RandomSearch', 'HillClimb') ,msg="Iterated Chaining fails for  odd no. of iterations")
 
     def test_iter_even(self):
-        self.assertIsNotNone(IteratedChaining(rounds=4, n_obs=2, tol=90).run('RandomSearch', 'HillClimb'))
+        self.assertIsNotNone(IteratedChaining(rounds=4, n_obs=2, tol=90).run('RandomSearch', 'HillClimb'),msg="Iterated Chaining fails for  even no. of iterations")
 if __name__ == '__main__':
     read_file('flights.txt')
     unittest.main()
