957e0c6ded50e2014a2421c041da7c6ce33f3570
Ankit Grover <agrover112@gmail.com>
2021-11-19 10:42:56 +0530

    feat: Added GARandomSearchReversals

--- fliscopt/ga.py
+++ fliscopt/ga.py
@@ -369,6 +369,128 @@ class GAReversals(BaseGA):
                     return costs[0][1], costs[0][0], scores, nfe, self.seed
 
         return costs[0][1], costs[0][0], scores, nfe, self.seed
+        
+class GARSReversals(BaseGA):
+     
+    """ 
+    A genetic algorithm that can perform reverse optimization, to escape local minimma inspired from [1]
+    In this algorithm the minimzation process is reversed to a maxmimization process i/n_k times i.e process in reverse direction for step_length no. of times.
+    Unlike GAReversals we perform a RandomSearch in reverse direction to escape local minimas.
+    
+    References:
+    
+    [1]https://www.microsoft.com/en-us/research/blog/genetic-algorithm-in-reverse-mode/#:~:text=%20To%20summarize%3A%20%201%20Reversing%20genetic%20algorithm,Duration%20and%20frequency%20of%20reversal%20cycle...%20More%20
+    
+    Attributes:
+        domain (list): List containing the upper and lower bound.i.e domain of our inputs
+        fitness_function (function): This parameter accepts a fitness function of given optimization problem.
+        seed (int,optional): Set the seed value of the random seed generator. Defaults to random integer value.
+        seed_init(bool,optional): True set's the seed of only population init generator, False sets all generators
+        init (list, optional): List for initializing the initial solution. Defaults to [].
+        epochs (int, optional): Number of times the algorithm runs. Defaults to 100.
+        
+        n_k (int, optional): The denominator factor i/n_k which determines the number of iterations which are multiples of n_k where reversals take place.Defaults to 250.
+        step_length (int, optional): The number of reversals steps/iterations to perform.Defeaults to 100 reversal steps.
+    Returns:
+        list: List containing the best_solution,
+        int: The final cost after running the algorithm,
+        list: List containing all costs during all epochs.
+        int: The number of function evaluations(NFE) after running the algorithm
+        int: Seed value used by random generators.
+    """
+    def __init__(self, domain=domain['domain'], fitness_function=fitness_function, seed=random.randint(10, 100), seed_init=True, init=[],max_time=100,
+                 population_size=100, step=1, probability_mutation=0.2, elitism=0.2,
+                 number_generations=500, search=False,n_k=250, step_length=100,) -> None:
+        super().__init__(domain, fitness_function, seed, seed_init, init,max_time, population_size, step, probability_mutation,
+                         0.0, elitism, number_generations, search)
+        self.n_k = n_k
+        self.step_length = step_length
+    
+
+    def run(self,domain,fitness_function,seed) -> tuple:
+        #self.__init__(domain, fitness_function, seed, self.seed_init, self.init,self.max_time)
+        super().__init__(domain, fitness_function, seed, self.seed_init, self.init,self.max_time, self.population_size, self.step, self.probability_mutation,
+                         0.0, self.elitism, self.number_generations, self.search)
+        population = []
+        scores = []
+        nfe = 0
+        rev = 0
+    
+        for i in range(self.population_size):
+            if self.search == True:
+                solution, b_c, sc, r_nfe, s = RandomSearch(
+                    ).run(self.domain, self.fitness_function,self.seed)
+                nfe += r_nfe
+            if len(self.init) > 0:
+                solution = self.init
+            else:
+                solution = [self.r_init.randint(self.domain[i][0], self.domain[i][1])
+                            for i in range(len(self.domain))]
+
+            population.append(solution)
+
+        number_elitism = int(self.elitism * self.population_size)
+        self.start_time=time.time()
+        for i in range(self.number_generations):
+            if not self.fitness_function.__name__ == 'fitness_function':
+                costs = [(self.fitness_function(individual), individual)
+                        for individual in population]
+            else:
+                costs = [(self.fitness_function(individual, 'FCO'), individual)
+                        for individual in population]
+            nfe += 1           
+            if i % self.n_k == 0 and i != 0:
+                if self.step_length == 1:
+                    costs.sort(reverse=True)
+                    rev += 1
+                else:
+                    rev += 1
+                    for _ in range(self.step_length - 1):
+                        costs.sort(reverse=True)
+                        nfe = 0
+                        solution = [self.r_init.randint(self.domain[i][0], self.domain[i][1])
+                                        for i in range(len(self.domain))]
+
+                        if not self.fitness_function.__name__ == 'fitness_function':
+                            cost = self.fitness_function(solution)
+                            #scores.append(cost)
+                        else:
+                            cost = self.fitness_function(solution, 'FCO')
+                            #scores.append(cost)
+                        
+                        nfe += 1
+                        if cost > self.best_cost:
+                            self.best_cost = cost
+                            self.best_solution = solution
+                            
+                        scores.append(self.best_cost)
+                        population.append(self.best_solution)
+
+                print(rev)  # To print the number of reversals
+            else:
+                heapq.heapify(costs)
+            ordered_individuals = [individual for (cost, individual) in costs]
+            population = ordered_individuals[0:number_elitism]
+            if not self.fitness_function.__name__ == 'fitness_function':
+                scores.append(self.fitness_function(population[0]))
+            else:
+                scores.append(self.fitness_function(population[0], 'FCO'))
+            nfe += 1
+            while len(population) < self.population_size:
+                if random.random() < self.probability_mutation:
+                    i1 = random.randint(0, number_elitism)
+                    i2 = random.randint(0, number_elitism)
+                    population.append(
+                        crossover(self.domain, ordered_individuals[i1], ordered_individuals[i2]))
+                else:
+                    m = random.randint(0, number_elitism)
+                    population.append(
+                        mutation(self.domain, self.step, ordered_individuals[m]))
+                        
+            if time.time()-self.start_time>self.max_time:
+                    return costs[0][1], costs[0][0], scores, nfe, self.seed
+
+        return costs[0][1], costs[0][0], scores, nfe, self.seed
     
  
 if __name__ == '__main__':
